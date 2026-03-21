512b4b31fd2920b6e7be48e247629da0de42e1ef
Saqlain Malik <saqlainmalik@maxifysolution.com>
2026-03-21 23:33:08 +0530

    by malik

--- adserabackend/models/authmodel.js
+++ adserabackend/models/authmodel.js
@@ -1,41 +1,52 @@
 const mongoose = require('mongoose');
 
 const userschema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true
-        },
-        mobile: {
-            type: String,
-            required: true
-        },
-        role: {
-            enum: ['admin', 'user'],
-            default: 'user',
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: true
-        },
-     lastLogin: {
-            date: Date,
-            ip: String,
-            device: String,
-            os: String,
-            browser: String
-        }
-
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
+
+    email: {
+      type: String,
+      required: true,
+    },
+
+    mobile: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      enum: ['admin', 'user'],
+      default: 'user',
+      type: String,
+      required: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    // ✅ NEW FIELD
+    status: {
+      type: String,
+      enum: ['active', 'blocked'],
+      default: 'active',
+    },
+
+    lastLogin: {
+      date: Date,
+      ip: String,
+      device: String,
+      os: String,
+      browser: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const User = mongoose.model('users', userschema);
