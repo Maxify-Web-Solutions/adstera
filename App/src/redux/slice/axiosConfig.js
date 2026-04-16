8dd40b92db43b4a56a7c01a79880f077aca4bc5f
Vikram Thakur <trivikramkumar@maxifysolution.com>
2026-04-16 14:18:44 +0530

    port changed

--- App/src/redux/slice/axiosConfig.js
+++ App/src/redux/slice/axiosConfig.js
@@ -1,7 +1,7 @@
 // api.js
 import axios from "axios";
 
-// const API_BASE_URL = "http://localhost:5000/api";
+// const API_BASE_URL = "http://localhost:5002/api";
 const API_BASE_URL = "/api";
 
 const api = axios.create({
