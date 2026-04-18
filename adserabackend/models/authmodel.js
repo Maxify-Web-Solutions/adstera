740f901c0626a4f9cd6998186ff27e42458fe48b
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-04-18 10:24:07 +0530

    by malik

--- adserabackend/models/authmodel.js
+++ adserabackend/models/authmodel.js
@@ -48,31 +48,6 @@ const userschema = new mongoose.Schema(
     // =========================
 
     // Unique referral code for each user
-    referralCode: {
-      type: String,
-      unique: true,
-    },
-
-    // Who referred this user
-    referredBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'users',
-      default: null,
-    },
-
-    // List of users this user referred
-    referrals: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'users',
-      },
-    ],
-
-    // Optional: earnings from referrals
-    referralEarnings: {
-      type: Number,
-      default: 0,
-    },
 
     // =========================
 
@@ -90,12 +65,7 @@ const userschema = new mongoose.Schema(
 );
 
 // 🔥 Auto-generate referral code before saving
-userschema.pre('save', function (next) {
-  if (!this.referralCode) {
-    this.referralCode = this._id.toString().slice(-6);
-  }
-  next();
-});
+
 
 const User = mongoose.model('users', userschema);
 
